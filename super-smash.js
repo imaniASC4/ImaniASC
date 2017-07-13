7d6296c439db3ca541c9aff0c04dc023ba712d93
ImaniASC4 <imani-10@live.com>
2017-07-13 14:09:46 -0400

    Finished super smash bros

--- super-smash.js
+++ super-smash.js
@@ -1,4 +1,12 @@
-var prompt=require('prompt-sync')();
+var ask =require('prompt-sync')();
 var masharray = ["dungeon","school","earth","mars","there"];
+for (var s = 0; s<masharray.length;s++){
+    console.log( (s+1) + masharray[s]);
+}
+var fightcenter = ask("which center do u choose to fight at? Pick a number 1 through " + masharray.length);
 var firstquestion=[0,1,2];
+console.log("Welcome to "+ masharray[fightcenter]);
 var secondquestion=["luigi","mario"];
+var firstanswer = ask("How many times do u wanna strike? (maximum amount of strikes at once is three)");
+var secondanswer = ask("Who do u want to fight?");
+console.log("You hit "+ secondanswer+ firstquestion[firstanswer] +"times. Too bad, Mewtwo wins.");//whos mewtwo
