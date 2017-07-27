4fedb2af1435fe2b641c076b4dd07905d1305b11
ImaniASC4 <imani-10@live.com>
2017-07-27 10:45:05 -0400

    Websites

---
+++ ogrethingy/ogre.js
@@ -0,0 +1,12 @@
+var playerHP=100;
+var playerGold=100;
+var playerLoose =0;
+var picx=400;
+var picy=400;
+function setup(){
+$("body").append("<h1> Welcome to ogre valley</h1>");
+$("body").append("HP: "+playerHP+" / Gold "+playerGold + " / player Points "+playerLoose);
+$("body").append("<img src='https://unsplash.it/"+picx+"/"+picy>"");
+}
+$(document).ready(setup);
+
